4bb999ed12d92c0265484685d2aa70b54c9d7ef6
ciscoadiz <ciscoadiz@gmail.com>
2022-04-25 20:45:28 +0100

    creado delete by id para controlador comment.js

--- controllers/comment.js
+++ controllers/comment.js
@@ -33,8 +33,10 @@ const findAll = async (req, res) => {
 
 const findById = async (req, res) => {
     const { id } = req.params;
+
     try {
         const comment = await Comment.findOne({ id });
+
         if (!comment) {
             res.status(404).send(notFound('Comment', id));
             return;
@@ -44,11 +46,25 @@ const findById = async (req, res) => {
     }catch (e) {
         res.status(500).send(internalServerError(e))
     }
+};
 
+const deleteById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletedComment = await Comment.findByIdAndDelete(id);
+        if (!deletedComment) {
+            res.status(404).send(notFound('Comment', id));
+            return;
+        }
+        res.status(200).send(deletedComment);
+    }catch (e) {
+        res.status(500).send(internalServerError(e))
+    }
 }
 
 module.exports = {
     create,
     findAll,
-    findById
+    findById,
+    deleteById
 };
--- routes/comment.js
+++ routes/comment.js
@@ -8,5 +8,6 @@ const api = express.Router();
 api.post('/comment', CommentController.create);
 api.get('/comment', CommentController.findAll);
 api.get('/comment/:id', CommentController.findById);
+api.delete('/comment/:id', CommentController.deleteById);
 
 module.exports = api;
