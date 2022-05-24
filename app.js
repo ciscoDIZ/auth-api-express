76e9ed177686c6b21569110332c7ad5cec7e2236
ciscoadiz <ciscoadiz@gmail.com>
2022-05-24 23:00:15 +0100

    cors

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(morgan('combined'));
-app.use(cors({origin: "*"}));
+app.use(cors());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
 app.locals.env = app.get('env');
